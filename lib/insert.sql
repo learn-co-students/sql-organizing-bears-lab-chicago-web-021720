fa2933b7d09ac9cae73042ca60ce7a0e55dc667e
owenabbott <ocabbott@gmail.com>
2020-02-27 14:20:43 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (2, "Hana", 1, "Tabby", 21800);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (3, "Grumpy Cat", 4, "Persian", 181600);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (4, "Lil\' Bub", 2, "Tortoiseshell", 2000000);
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,'Mr. Chocolate', 28, 'M', 'brown', 'Well fed and happy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Rowdy', 10, 'F', 'black', 'jovial', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Tabitha', 12, 'M', 'brown', 'mean', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Sergeant Brown', 30, 'F', 'neon blue', 'happy', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Melissa', 2000, 'F', 'grey', 'has seen beyond the end of time', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, 'Grinch', 9, 'M', 'gold', 'hungry', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, 'Wendy', 11, 'M', 'purple', 'mean', 1);
+INSERT INTO bears (id, age, gender, color, temperament, alive) VALUES (8, 28, 'M', 'brown', 'happy', 0);
+
+
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER,
+-- gender TEXT,
+-- color TEXT
+-- temperment TEXT
+-- alive BOOLEAN
+-- );
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,45 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT NAME, AGE FROM bears WHERE gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT NAME FROM bears ORDER BY name"
+
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT NAME, AGE FROM bears WHERE ALIVE = 1 ORDER BY age"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT NAME, AGE from bears ORDER BY age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT NAME, AGE from bears ORDER BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(*) 
+  FROM bears
+  GROUP BY color
+  ORDER BY 2 DESC
+  LIMIT 1";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT (temperament) 
+  FROM bears 
+  WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name IS null"
 end
+
+
+
+
+# GOOD SQL:
+# SELECT employee, SUM(bonus) FROM employee_bonus
+# GROUP BY employee HAVING SUM(bonus) > 1000;
